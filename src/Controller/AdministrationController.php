270b2d1b2108d6395c4ecb97b22a6470bf0c902d
Guillaume CAVALIE <actix2013@gmail.com>
2020-04-25 16:06:24 +0200

    INITIAL COMMIT

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 use App\Model\ItemManager;
+use App\Model\PartManager;
 
 class AdministrationController extends AbstractController
 {
@@ -24,4 +25,12 @@ class AdministrationController extends AbstractController
     {
         return $this->twig->render('Administration/indexno.html.twig');
     }
+
+    public function showpart()
+    {
+        $partManager = new PartManager();
+        $parts = $partManager->selectAll();
+        return $this->twig->render('Administration/indexno.html.twig', ['items' => $parts]);
+    }
+
 }
