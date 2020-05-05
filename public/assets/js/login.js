3a157d661b58a74644d11d7213ac78a4c2e672e0
Ben2669 <benjamin.moniotte@gmail.com>
2020-05-05 15:36:45 +0200

    création du formulaire sign in, séléction formulaire possible en fonction de la tab cliquée (js)

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -18,3 +18,25 @@ function showSlides()
     dots[slideIndex-1].className += " bnj-login-dot-active";
     setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
+
+function showTab(evt, tabButton) {
+    let i = 0;
+    let tabContentContainer = document.getElementsByClassName('bnj-form-content');
+    let tab = document.getElementsByClassName('bnj-tab');
+
+    for (i = 0; i < tabContentContainer.length; i++) {
+        tabContentContainer[i].style.display = "none";
+    }
+
+    for (i = 0; i < tab.length; i++) {
+        tab[i].className = tab[i].className.replace(" bnj-tab-active", "");
+    }
+
+    document.getElementById(tabButton).style.display = "flex";
+    document.getElementById(tabButton).style.flexDirection = "column";
+    document.getElementById(tabButton).style.justifyContent = "space-evenly";
+    document.getElementById(tabButton).style.alignItems = "center";
+
+    evt.currentTarget.className += " bnj-tab-active";
+
+}
