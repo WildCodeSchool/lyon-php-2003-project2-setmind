a61bd4665f32404542eccfc791bf13bc14dfc676
Ben2669 <benjamin.moniotte@gmail.com>
2020-05-06 18:48:37 +0200

    gestion d'erreur du form sign up

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -17,36 +17,53 @@ class LoginController extends AbstractController
      */
     public function connexion()
     {
-        if ($_POST) {
-            if (!empty($_POST)) {
+        return $this->twig->render('Login/connexion.html.twig');
+    }
+
+    public function addUser()
+    {
+        if (!empty($_POST)) {
+            $error = [];
+            $user = [];
+            if (!empty($_POST['first_name'])) {
+                $userFirstName = trim($_POST['first_name']);
+                $user['first_name'] = $userFirstName;
+            } else {
+                $error['firstName'] = "Please insert your first name here!";
+            }
+            if (!empty($_POST['last_name'])) {
+                $userLastName = trim($_POST['last_name']);
+                $user['last_name'] = $userLastName;
+            } else {
+                $error['lastName'] = "Please insert your last name here!";
+            }
+            if (!empty($_POST['email'])) {
+                if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                    $userEmail = $_POST['email'];
+                    $user['email'] = $userEmail;
+                } else {
+                    $error['email'] = "Please insert a valid email format!";
+                }
+            } else {
+                $error['email'] = "Please insert your email here!";
+            }
+            if (!empty($_POST['password'])) {
+                if (empty($error)) {
+                    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+                    $user['password'] = $password;
+                } else {
+                    $user['password'] = $_POST['password'];
+                }
+            } else {
+                $error['password'] = "Please insert your password here!";
+            }
+            if (!empty($error)) {
+                return $this->twig->render('Login/connexion.html.twig', ['error' => $error, 'user' => $user]);
+            } else {
                 $loginManager = new LoginManager();
-                $user = [
-                    'first_name' => $_POST['first_name'],
-                    'last_name' => $_POST['last_name'],
-                    'email' => $_POST['email'],
-                    'password' => $_POST['password']
-                ];
                 $loginManager->signUp($user);
                 return $this->twig->render('Login/connexion.html.twig', ['user' => $user]);
             }
-        } else {
-            return $this->twig->render('Login/connexion.html.twig');
         }
     }
-
-    /*public function addUser()
-    {
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $loginManager = new LoginManager();
-            $user = [
-                'first_name' => $_POST['first_name'],
-                'last_name' => $_POST['last_name'],
-                'email' => $_POST['email'],
-                'password' => $_POST['password']
-            ];
-            $loginManager->signUp($user);
-            return $this->twig->render('Login/connexion.html.twig', ['user' => $user]);
-        }
-    }*/
 }
