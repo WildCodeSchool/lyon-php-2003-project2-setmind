2aace451f2fdf0dfedcd20b67025735243fcd8d5
Ben2669 <benjamin.moniotte@gmail.com>
2020-05-11 22:59:37 +0200

    redirection vers la page envelop/resultat quand on submit pour la dernière fois

--- src/Controller/QuestionnaireController.php
+++ src/Controller/QuestionnaireController.php
@@ -6,12 +6,18 @@ use App\Model\QuestionnaireManager;
 
 class QuestionnaireController extends AbstractController
 {
+    const LASTANSWERS = [9, 10, 11, 12, 28, 29, 30, 31, 46, 47, 48, 49, 54, 55, 56, 57];
+
     public function questionnaire()
     {
         $questionnaireManager = new QuestionnaireManager();
         if (!empty($_POST["reponse_choisie_id"])) {
-            $question = $questionnaireManager->selectNextQuestion($_POST["reponse_choisie_id"]);
-            return $this->twig->render('Questionnaire/questionnaire.html.twig', ['question' => $question]);
+            if (in_array($_POST["reponse_choisie_id"], self::LASTANSWERS)) {
+                header('Location: /envelop/resultat');
+            } else {
+                $question = $questionnaireManager->selectNextQuestion($_POST["reponse_choisie_id"]);
+                return $this->twig->render('Questionnaire/questionnaire.html.twig', ['question' => $question]);
+            }
         }
         $defaultQuestion = 1;
         $question = $questionnaireManager->selectQuestion($defaultQuestion);
