7efe13eded82e558600a969d12a7bed0eff163ea
Guillaume CAVALIE <actix2013@gmail.com>
2020-05-11 12:17:03 +0200

    ADD EMPTY PAGE RESULTAT + MANAGER FOR ARRAY ID ENVELOPS

--- src/Controller/EnvelopController.php
+++ src/Controller/EnvelopController.php
@@ -21,4 +21,19 @@ class EnvelopController extends AbstractController
         $envelops = $envelopManager->selectAllWithParts();
         return $this->twig->render('Envelop/envelops.html.twig', ['envelops' => $envelops]);
     }
+
+
+    public function resultat()
+    {
+        if (!isset($_SESSION["user"])) {
+            header("location:/login/login");
+        }
+        // todo create profile manager
+        $envelopManager = new EnvelopManager();
+        // todo get list off id enveloppes from  prifile and build array with ids.
+        //$envelops = $envelopManager->selectOneWithPartsById(18);
+        // pass ids in array for wanted envelops ids
+        $envelops = $envelopManager->selectWithPartsByIds([18,12]);
+        return $this->twig->render('Envelop/resultat.html.twig', ['envelops' => $envelops]);
+    }
 }
