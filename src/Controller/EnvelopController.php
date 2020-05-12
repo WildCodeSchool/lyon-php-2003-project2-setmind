6989e050f8a6c0dc0f12aee805e247a833173118
Ben2669 <benjamin.moniotte@gmail.com>
2020-05-12 22:45:21 +0200

    crée ProfilManager, envoie l'id profil sur la page résultat en fonction des réponses du questionnaire

--- src/Controller/EnvelopController.php
+++ src/Controller/EnvelopController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Model\EnvelopManager;
+use App\Model\ProfilManager;
 
 class EnvelopController extends AbstractController
 {
@@ -29,11 +30,16 @@ class EnvelopController extends AbstractController
             header("location:/login/login");
         }
         // todo create profile manager
+        $profilManager = new ProfilManager();
         $envelopManager = new EnvelopManager();
         // todo get list off id envelops from  profile and build array with ids.
         //$envelops = $envelopManager->selectOneWithPartsById(18);
         // pass ids in array for wanted envelops ids
+        $resultValues = array_count_values($_SESSION["resultat"]);
+        arsort($resultValues);
+        $idProfil = array_slice(array_keys($resultValues), 0, 1, true);
+        $profil = $profilManager->selectOneById($idProfil[0]);
         $envelops = $envelopManager->selectWithPartsByIds([18,12]);
-        return $this->twig->render('Envelop/resultat.html.twig', ['envelops' => $envelops]);
+        return $this->twig->render('Envelop/resultat.html.twig', ['envelops' => $envelops, 'profil' => $profil]);
     }
 }
